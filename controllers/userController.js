746fb9974d03290ec6454dd21dd6487062d3c716
Kaung Shein <nicholasling1412@gmail.com>
2021-04-19 16:49:24 -0400

    VerifyToken fixed bug + Function to add item to specific user Id

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,14 +4,42 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
 const User = require("../models/user");
+const Item = require("../models/item");
 
 const { registerValidation, loginValidation } = require("../validation");
+const verifyToken = require("./verifyToken");
+
+// Add an item to specific user by ID
+router.get("/item/create", verifyToken, async (req, res) => {
+  const userId = req.user._id;
+  User.findById(userId)
+    .populate("items")
+    .then((user) => {
+      Promise.all([
+        Item.create({
+          name: req.body.name,
+          type: req.body.type,
+          quantity: req.body.quantity,
+          obtainedDate: req.body.obtainedDate,
+        }).then((item) => {
+          user.items.push(item);
+        }),
+      ])
+        .then(() => {
+          user.save();
+        })
+        .then(() => {
+          res.json(user);
+        });
+    })
+    .catch((error) => res.json({ status: 400, error: error }));
+});
 
 //* Register a User
 router.post("/register", async (req, res) => {
   // Validates the information
   const { error } = registerValidation(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
+  if (error) return res.status(400).json(error.details[0].message);
 
   // Check if Email exists in database
   const emailExist = await User.findOne({ email: req.body.email });
@@ -19,18 +47,18 @@ router.post("/register", async (req, res) => {
 
   // Hash passwords
   const salt = await bcrypt.genSalt(10);
-  const hashPassword = await bcrypt.hash(req.body.password, salt);
+  const hashedPassword = await bcrypt.hash(req.body.password, salt);
 
   // Create a new User
   const user = new User({
     username: req.body.username,
     email: req.body.email,
-    password: req.body.password,
+    password: hashedPassword,
     items: [],
   });
   try {
     const savedUser = await user.save();
-    res.send("Account successfully created");
+    res.send("User registration successful!");
   } catch (err) {
     res.status(400).send(err);
   }
--- controllers/verifyToken.js
+++ controllers/verifyToken.js
@@ -1,6 +1,6 @@
 const jwt = require("jsonwebtoken");
 
-const auth = (req, res, next) => {
+const verifyToken = (req, res, next) => {
   const token = req.header("auth-token");
   if (!token) return res.status(401).send("Access Denied");
 
