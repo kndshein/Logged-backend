1990e06747273b527205684004e6c4e6e36eab6d
Kaung Shein <nicholasling1412@gmail.com>
2021-04-18 20:16:36 -0400

    Authorization setup continued

--- controllers/userController.js
+++ controllers/userController.js
@@ -20,6 +20,7 @@ router.post("/register", async (req, res) => {
   const salt = await bcrypt.genSalt(10);
   const hashPassword = await bcrypt.hash(req.body.password, salt);
 
+  // Create a new User
   const user = new User({
     username: req.body.username,
     email: req.body.email,
